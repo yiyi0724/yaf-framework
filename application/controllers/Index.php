6c1c62198b6db6267cf5b0012e43c844b38ccf26
enychen <346745114@qq.com>
2015-12-15 16:36:36 +0800

    更新邮件类

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -32,11 +32,9 @@ class IndexController extends BaseController
 	}
 	
 	public function sendmailAction()
-	{
-		
-		$mail = new \Mail\PHPMailer();			// 创建一个邮件对象
-		
-		$mail->isHTML(true);                        // 内容使用html的方式
+	{		
+		$mail = new \Network\Mail();			// 创建一个邮件对象
+
 		$mail->isSMTP(); 							// 使用SMPT验证
 		$mail->Host = 'smtp.163.com';				// smtp服务器
 		$mail->SMTPAuth = true;                    	// 是否验证
@@ -52,6 +50,8 @@ class IndexController extends BaseController
 		$mail->Subject = '我在测试发送邮件';			// 邮件的标题
 		$mail->Body    = '甩卖妹子一枚, <b>yyq!</b>';	// 邮件的内容
 		$mail->AltBody = '为了两块钱我也是拼啊';		// 邮件的备注
+		
+		$mail->isHTML(true);                        // 内容使用html的方式
 
 		if(!$mail->send()) {
 			echo '发送失败: ' . $mail->ErrorInfo;
