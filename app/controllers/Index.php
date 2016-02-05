af1cb1d8d19ecf1f2560e8503594e162db63ad6e
enychen <346745114@qq.com>
2016-02-05 11:21:16 +0800

    暂存

--- app/controllers/Index.php
+++ app/controllers/Index.php
@@ -4,12 +4,5 @@ class IndexController extends \Base\AppController
 {
 	public function indexAction()
 	{
-		$data = $this->validate();
-		echo '<pre>';
-		print_r($data);
-		exit;
-/* 		$addLogic = new \logic\Member\Add();
-		return $this->jsonp($addLogic->getUserInfo());
-		exit; */
 	}
 }
