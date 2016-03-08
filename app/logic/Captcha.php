3082a3c6860ff54b8a11d881155be4d0d0ecc2b1
enychen <346745114@qq.com>
2016-03-08 11:20:27 +0800

    初始化yaf框架

---
+++ app/logic/Captcha.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * 验证码操作逻辑
+ * @author enychen
+ */
+namespace logic;
+
+class Captcha extends Logic
+{	
+	/**
+	 * 把验证码写到session中
+	 * @param string $channel 验证码频道
+	 * @param string $code 验证码
+	 */
+	public function setCaptchaToSession($channel, $code)
+	{
+		$this->getSession()->set($channel, $code);
+	}
+	
+	/**
+	 * 检查登录验证码是否正确
+	 * @param string $channel 验证码频道
+	 * @param string $code 验证码
+	 */
+	public function checkCodeFromSession($channel, $code)
+	{
+		$sessionCode = $this->getSession()->get($channel);
+		if($code == $sessionCode)
+		{
+			$this->getSession()->del($channel);
+			return TRUE;
+		}
+		
+		return FALSE;
+	}
+}
