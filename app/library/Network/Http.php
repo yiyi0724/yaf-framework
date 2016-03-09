0a39f045e8b0162a17aaa8be55c959d992410fd7
enychen <346745114@qq.com>
2016-03-09 19:50:14 +0800

    初始化yaf框架

--- app/library/Network/Http.php
+++ app/library/Network/Http.php
@@ -7,8 +7,7 @@
  */
 namespace Network;
 
-class Http
-{
+class Http {
 	/**
 	 * json解析
 	 * @var const
@@ -40,46 +39,101 @@ class Http
 	protected $result = NULL;
 
 	/**
-	 * 结果解析
-	 * @var bool
+	 * 结果解析类型
+	 * @var int
 	 */
 	protected $decode = NULL;
 
 	/**
 	 * 构造函数
-	 * @param string $action 要请求的url地址
-	 * @param int $decode 是否要解析，只支持xml或者json解析方式, json: \Network\Http::DECODE_JSON, xml: \Network\Http::DECODE_XML
+	 * @param int $timeout 超时时间，默认60秒
 	 * @param bool $return 结果是否返回，如果不返回则直接输出，默认返回不输出
 	 * @param bool $header　启用时会将头文件的信息作为数据流输出, 默认不输出
+	 * @return void
 	 */
-	public function __construct($action, $decode = NULL, $return = TRUE, $header = FALSE)
-	{
+	public function __construct($timeout = 60, $return = TRUE, $header = FALSE) {
 		// 创建连接资源
 		$this->curl = curl_init();
-		// url地址
+		
+		// 设置头信息输出/结果返回/超时时间
+		$this->setCurlopt(CURLOPT_RETURNTRANSFER, $return);
+		$this->setCurlopt(CURLOPT_HEADER, $header);
+		$this->setCurlopt(CURLOPT_TIMEOUT, $timeout);
+	}
+	
+	/**
+	 * 设置要请求的url地址
+	 * @param string $action url地址
+	 * @return void
+	 */
+	public function setAction($action) {
 		$this->action = $action;
-		// 设置选项,不输出头信息和结果返回
-		curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, $return);
-		curl_setopt($this->curl, CURLOPT_HEADER, $header);
-		// 结果是否要解析,如果需要返回或者需要输出头信息,则不进行解析,因为解析一定失败
-		$this->decode = (!($return && $header)) && $decode ? $decode : NULL;
-		// curl超时时间
-		$http->setCurlopt(CURLOPT_TIMEOUT, 30);
+	}
+
+	/**
+	 * 设置结果解析
+	 * @param string $decode 只支持 \Network\Http::DECODE_JSON 或者 \Network\Http::DECODE_XML
+	 * @return void
+	 */
+	public function setDecode($decode) {
+		$this->decode = $decode;
+	}
+
+	/**
+	 * 设置cookie信息
+	 * @param string|array $cookie cookie信息
+	 * @return void
+	 */
+	public function setCookie($cookie) {
+		if(is_array($cookie)) {
+			$format = array();
+			foreach($cookie as $key=>$val) {
+				$format[] = "{$key}={$val}";
+			}
+			$cookie = implode('; ', $format);
+		}
+		$this->setCurlopt(CURLOPT_COOKIE, $cookie);
+	}
+
+	/**
+	 * 设置curlopt的设置
+	 * @param string $key CURLOPT_*设置选项,参照http://php.net/manual/zh/function.curl-setopt.php
+	 * @param int|string|bool $value 值
+	 * @return void
+	 */
+	public function setCurlopt($key, $value) {
+		curl_setopt($this->curl, $key, $value);
+	}
+
+	/**
+	 * 上传文件的创建方式
+	 * @param string $path 文件的绝对路径
+	 * @return \CURLFile|string 上传文件对象或者字符串
+	 */
+	public function getFile($path) {
+		return class_exists('\CURLFile') ? new \CURLFile($path) : "@{$path}";
+	}
+	
+	/**
+	 * 关闭curl资源
+	 * @return void
+	 */
+	public function close() {
+		curl_close($this->curl);
 	}
 
 	/**
 	 * 执行请求
-	 * @param string $method 请求方法,get|post|put|delete|upload
+	 * @param string $method 请求方法，get|post|put|delete|upload
 	 * @param array $fields 要传递的参数
-	 * @return bool 请求成功或失败
+	 * @return bool|string 请求的结果，如果返回信息则返回请求字符串
 	 */
-	public function __call($method, $fields)
-	{
+	public function __call($method, $fields) {		
 		// 读取数据
-		$fields = isset($fields[0]) ? $fields[0] : array();
+		$fields = array_shift($fields);
+		
 		// 进行操作
-		switch($method)
-		{
+		switch($method) {
 			case 'get':
 				$fields and ($this->action = "{$this->action}?" . http_build_query($fields));
 				break;
@@ -87,34 +141,33 @@ class Http
 			case 'put':
 			case 'delete':
 				curl_setopt($this->curl, CURLOPT_CUSTOMREQUEST, strtoupper($method));
-				curl_setopt($this->curl, CURLOPT_HTTPHEADER, array(
-					'Content-Length: ' . mb_strlen($fields)
-				));
-				curl_setopt($this->curl, CURLOPT_POSTFIELDS, http_build_query($fields));
+				if($fields) {
+					$fields = http_build_query($fields);
+					curl_setopt($this->curl, CURLOPT_HTTPHEADER, array('Content-Length: ' . mb_strlen($fields)));
+					curl_setopt($this->curl, CURLOPT_POSTFIELDS, $fields);
+				}
 				break;
 			case 'upload':
 				curl_setopt($this->curl, CURLOPT_POST, TRUE);
 				curl_setopt($this->curl, CURLOPT_POSTFIELDS, $fields);
 				break;
 			default:
-				throw new \Exception("Not Found Method Http::{$method}()", 90000);
+				throw new \Exception("Not Found Method Http::{$method}()", 99000);
 		}
+		
 		// 设置请求的地址
 		curl_setopt($this->curl, CURLOPT_URL, $this->action);
+		
 		// 执行请求
 		$this->result = curl_exec($this->curl);
+				
 		// 操作失败
-		if($this->result === FALSE)
-		{
-			throw new \Exception("Connect Error", 90001);
+		if($this->result === FALSE) {
+			throw new \Exception("Curl Exec Error", 99001);
 		}
+		
 		// 进行解析
-		if(in_array($this->decode, array(static::DECODE_JSON, static::DECODE_XML)))
-		{
-			$this->decode();
-		}	
-		// 关闭连接
-		curl_close($this->curl);
+		$this->decode and $this->decode($this->result);
 		
 		// 返回结果
 		return $this->result;
@@ -125,75 +178,22 @@ class Http
 	 * @param string $result curl的返回结果
 	 * @throws \Exception
 	 */
-	protected function decode()
-	{
+	protected function decode($result) {
 		// xml解析
-		if($this->decode == static::DECODE_XML)
-		{
-			$result = @simplexml_load_string($this->result, 'SimpleXMLElement', LIBXML_NOCDATA);
-			if(!$result)
-			{
-				throw new \Exception("Decode Error", 90002);
+		if($this->decode == static::DECODE_XML) {
+			$result = @simplexml_load_string($result, 'SimpleXMLElement', LIBXML_NOCDATA);
+			if(!$result) {
+				throw new \Exception("XML Decode Error", 99002);
 			}
-			$this->result = json_encode($result);
+			$result = json_encode($result);
 		}
 		
-		// 通用解析,其实是json解析
-		// 因为如果是xml,则会先把xml进行json_encode,所以此处可以直接json_decode
-		$this->result = json_decode($this->result, TRUE);
-		if(json_last_error())
-		{
-			throw new \Exception("DECODE ERROR, ORIGIN DATA:{$result}", 90002);
+		// json解析
+		$result = json_decode($result, TRUE);
+		if(json_last_error()) {
+			throw new \Exception("Decode Error", 99002);
 		}
-	}
-
-	/**
-	 * 设置头信息
-	 * @param array|string $header 头信息数组
-	 */
-	public function setHeader($headers)
-	{
-		curl_setopt($this->curl, CURLOPT_HTTPHEADER, is_array($headers) ? $headers : array(
-			$headers
-		));
-	}
-
-	/**
-	 * 设置cookie
-	 * @param string|array $cookie cookie信息
-	 */
-	public function setCookie($origin)
-	{
-		$cookie = $origin;
-		if(is_array($origin))
-		{
-			$cookie = array();
-			foreach($origin as $key=>$val)
-			{
-				$cookie[] = "{$key}={$val}";
-			}
-			$cookie = implode('; ', $cookie);
-		}
-		curl_setopt($this->curl, CURLOPT_COOKIE, $cookie);
-	}
-
-	/**
-	 * 设置curlopt的设置
-	 * @param string $key CURLOPT_*设置选项,参照http://php.net/manual/zh/function.curl-setopt.php
-	 * @param mixed $value 值
-	 */
-	public function setCurlopt($key, $value)
-	{
-		curl_setopt($this->curl, $key, $value);
-	}
-
-	/**
-	 * 上传文件的创建方式
-	 * @param string $path 文件的绝对路径
-	 * @return \CURLFile|string 上传文件对象或者字符串
-	 */
-	public function getFile($path)
-	{
-		return class_exists('\CURLFile') ? new \CURLFile($path) : "@{$path}";
+		
+		$this->result = $result;
 	}
 }
