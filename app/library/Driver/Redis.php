a9db238dcdcb119e0a2b1220bc094735a033f059
enychen <346745114@qq.com>
2016-03-09 17:51:26 +0800

    初始化yaf框架

--- app/library/Driver/Redis.php
+++ app/library/Driver/Redis.php
@@ -19,26 +19,24 @@ class Redis extends Driver {
 	 * 创建对象
 	 * @param array $driver 配置数组 host | port | timeout | auth | db | options
 	 * @throws \RedisException
+	 * @return void
 	 */
 	protected function __construct(array $driver) {
-		// 创建redis对象
-		$this->redis = new \Redis();
-		// 连接redis
-		if($this->redis->pconnect($driver['host'], $driver['port'], $driver['timeout'])) {
-			// 是否需要验证密码
-			$driver['auth'] and $this->redis->auth($driver['auth']);
-			// 是否需要选择数据库
-			$driver['db'] and $this->redis->select($driver['db']);
+		try {
+			// 创建redis对象
+			$this->redis = new \Redis();
 			// 选项设置
-			if(isset($driver['options'])) {
-				foreach($driver['options'] as $key=>$option) {
-					$this->redis->setOption(constant("\Redis::OPT_" . strtoupper($key)), $option);
-				}
+			foreach($driver['options'] as $key=>$option) {
+				$this->redis->setOption(constant("\Redis::OPT_" . strtoupper($key)), $option);
 			}
-		}
-		else {
-			// 连接失败抛出错误
-			throw new \RedisException("Redis Connection Error: {$driver['host']}:{$driver['port']}");
+			// 持久性连接
+			$this->redis->pconnect($driver['host'], $driver['port'], $driver['timeout']);
+			// 选择数据库
+			$this->redis->select($driver['dbname']);
+			// 密码
+			$driver['auth'] and $this->redis->auth($driver['auth']);
+		} catch(\Exception $e) {
+			throw new \RedisException("Redis Connect Error: {$e->getMessage()}");
 		}
 	}
 
@@ -46,11 +44,9 @@ class Redis extends Driver {
 	 * 静态调用方式
 	 * @param string $method 方法名
 	 * @param array $args 参数
+	 * @return mixed
 	 */
 	public function __call($method, $args) {
-		return call_user_func_array([
-			$this->redis, 
-			$method
-		], $args);
+		return call_user_func_array(array($this->redis, $method), $args);
 	}
 }
