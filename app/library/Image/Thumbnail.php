f6dda96e3e6e9e0dec8fa511ede85c08b1e6512f
陈晓波 <346745114@qq.com>
2016-05-11 16:50:55 +0800

    水印图

--- app/library/Image/Thumbnail.php
+++ app/library/Image/Thumbnail.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * 缩略裁剪图
  * @author enychen
@@ -7,335 +8,177 @@
 namespace Image;
 
 class Thumbnail {
-	
-	/**
-	 * 图片保存的目录
-	 * @var string
-	 */
-	private $path = './thumbs/';
-	
-	/**
-	 * 图片起始x标点
-	 * @var int
-	 */
-	private $srcx = 0;
-	
-	/**
-	 * 原图片起始y标点
-	 * @var int
-	 */
-	private $srcy = 0;
 
 	/**
-	 * 原始图片宽度
-	 * @var int
+	 * 源图
+	 * @var array
 	 */
-	private $srcwidth = 0;
-	
-	/**
-	 * 原始图片高度
-	 * @var int
-	 */
-	private $srcheight = 0;
-	
-	/**
-	 * 缩略图片宽度
-	 * @var int
-	 */
-	private $distwidth = 0;
-	
-	/**
-	 * 原始图片高度
-	 * @var int
-	 */
-	private $distheight = 0;
+	private $srcImage = NULL;
 
 	/**
-	 * 支持缩略图的格式
-	 * @var int
+	 * 保存图
+	 * @var array
 	 */
-	private $allowType = array(1=>'gif', 2=>'jpeg', 3=>'png', 4=>'wbmp', 5=>'webp', 6=>'xbm', 7=>'xpm');
-	
+	private $distImage = NULL;
+
 	/**
-	 * 原始图片格式
-	 * @var int
+	 * 错误信息列表
+	 * @var array
 	 */
-	private $srcType;
-	
+	private $errorNotify = array(
+		0=>'未知错误',
+		1=>'源图不存在',
+		2=>'源图格式不合法',
+		3=>'保存目录无操作权限',
+		4=>'图片格式不支持',
+	);
+
 	/**
-	 * 缩略图片格式
-	 * @var int
-	 */
-	private $disttype;
-	
-	/**
-	 * 原始图片路径
-	 * @var int
-	 */
-	private $srcfilename;
-	
-	/**
-	 * 缩略图存放地址
-	 * @var string
-	 */
-	private $distfilename;
-	
-	/**
-	 * 原图进行正方形缩略
-	 * @var unknown
-	 */
-	private $srcsquare = FALSE;
-	
-	/**
-	 * 错误代码
-	 * @var int
-	 */
-	private $errorCode;
-	
-	/**
-	 * 错误信息
-	 * @var int
-	 */
-	private $errorMsg;
-	
-	/**
-	 * 连贯操作成员属性($path, $srcx, $srcy, $srcwidth,$srcheight, $distwidth, $distheight, $srcfilename, $disttype);
-	 * @param string $key 属性名
-	 * @param mixed $val 属性值
-	 * @return \Image\Thumbnail 可以进行连续操作
-	 */
-	public function set($key, $val) {
-		$key = strtolower($key);
-		if(array_key_exists($key, get_class_vars(get_class($this)))) {
-			$this->setOption($key, $val);
-		}
-		return $this;
-	}
-	
-	/**
-	 * 设置单个属性值
-	 * @param string $key 属性名
-	 * @param mixed $val 属性值
+	 * 设置源图
+	 * @param string $srcFilename 原始图片名称
 	 * @return void
 	 */
-	private function setOption($key, $val) {
-		$this->$key = $val;
-	}
-	
-	/**
-	 * 创建缩略图
-	 * @return bool 创建成功返回TRUE
-	 */
-	public function create() {
-		// 检查文件是否存在
-		if(!$this->isWritable()) {
-			$this->setOption('errorMsg', $this->getError());
-			return FALSE;
+	private function setSrcFilename($srcFilename) {
+		// 图片是否存在
+		if(!is_file($srcFilename)) {
+			$this->throwError(1);
 		}
 
-		// 设置图片信息
-		if(!$this->setSrcInfo()) {
-			$this->setOption('errorMsg', $this->getError());
-			return FALSE;
+		// 分析图片信息
+		$imageInfo = @getimagesize($srcFilename);
+		if(!$imageInfo) {
+			$this->throwError(2);
 		}
-		
-		// 设置目标图片信息
-		if(!$this->setDistInfo()) {
-			$this->setOption('errorMsg', $this->getError());
-			return FALSE;
-		}
-	
-		// 生成缩略图
-		if(!$this->copy()) {
-			$this->setOption('errorMsg', $this->getError());
-			return FALSE;
-		}
-		
-		return TRUE;
-	}
-	
-	/**
-	 * 获取完整目标缩略图名称
-	 * @return string
-	 */
-	public function getDistFileName() {
-		return $this->distfilename;
-	}
-	
-	/**
-	 * 获取错误信息
-	 * @return string
-	 */
-	public function getErrorMsg() {
-		return $this->errorMsg;
-	}
-	
-	/**
-	 * 生成缩略图
-	 */
-	private function copy() {
-		// 载入旧的画布
-		$srcImage = call_user_func("imagecreatefrom{$this->srcType}", $this->srcfilename);
-		
-		// 创建新的画布
-		$distImage = imagecreatetruecolor($this->distwidth, $this->distheight);
-		switch(TRUE) {
-			case $this->disttype == 'png':
-				// 保存格式为png
-				imagesavealpha($srcImage, TRUE);		// 不要丢了$srcImage图像的透明色;
-				imagealphablending($distImage, FALSE);	// 不合并颜色,直接用$distImage图像颜色替换,包括透明色;
-				imagesavealpha($distImage, TRUE);		// 不要丢了$distImage图像的透明色;
-				break;
-			case $this->srcType == 'png' && $this->disttype != 'png':
-				// png转其他格式
-				$color = imagecolorallocate($distImage, 255, 255, 255);
-				imagefill($distImage, 0, 0, $color);
-				break;
-		}
-		
-		// 完整拷贝
-		imagecopyresampled($distImage, $srcImage, 0, 0, $this->srcx, $this->srcy, $this->distwidth, $this->distheight, $this->srcwidth, $this->srcheight);
-		
-		// 销毁原图资源
-		imagedestroy($srcImage);		
-		// 生成缩略图
-		call_user_func("image{$this->disttype}", $distImage, $this->distfilename);
-		imagedestroy($distImage);
 
-		return TRUE;
+		// 分析图片格式
+		$mineType = explode('/', $imageInfo['mime']);
+
+		// 保存信息
+		$this->srcImage['ext'] = $mineType[1];
+		$this->srcImage['width'] = $imageInfo[0];
+		$this->srcImage['height'] = $imageInfo[1];
+		$this->srcImage['resource'] = $this->openFile($srcFilename);
 	}
-	
+
 	/**
-	 * 根据错误代码转成错误信息
-	 * @return string
+	 * 设置目标图片
+	 * @param string $distFilename 目标图片名称
+	 * @return void
 	 */
-	private function getError() {
-		switch($this->errorCode) {
-			case -1:
-				$str = '文件不存在';
-				break;
-			case -2:
-				$str = '未设置保存目录';
-				break;
-			case -3:
-				$str = '保存目录不存在或无操作权限';
-				break;
-			case -4:
-				$str = '图片格式不支持转换';
-				break;
-			case -5:
-				$str = '图片格式不支持保存';
-				break;
-			case -6:
-				$str = '未设置保存文件名';
-				break;
-			case -7:
-				$str = '缩略图尺寸大于原图尺寸';
-				break;
-			default:
-				$str = '未知错误';
-		}
-		
-		return $str;
-	}
-	
-	/**
-	 * 目录是否可以写入(不存在尝试创建)
-	 * @return boolean
-	 */
-	private function isWritable() {
-		// 源文件是否存在
-		if(!is_file($this->srcfilename)) {
-			$this->setOption('errorCode', -1);
-			return FALSE;
-		}
-		
-		// 存储目录是否存在
-		if(empty($this->path)) {
-			$this->setOption('errorCode', -2);
-			return FALSE;
-		}
-		if(!is_dir($this->path) || !is_writable($this->path)) {
-			if(!@mkdir($this->path, 0755, TRUE)) {
-				$this->setOption('errorCode', -3);
-				return FALSE;
+	private function setDistFilename($distFilename) {
+		$distInfo = pathinfo($distFilename);
+		$path = $distInfo['dirname'] . DIRECTORY_SEPARATOR;
+		$ext = str_replace('jpg', 'jpeg', $distInfo['extension']);
+
+		// 保存目录判断是否存在
+		if(!is_dir($path) || !is_writable($path)) {
+			if(!@mkdir($path, 0755, TRUE)) {
+				$this->throwError(3);
 			}
 		}
-		return TRUE;
-	}
-	
-	/**
-	 * 原图片信息分析
-	 * @return boolean 分析成功返回TRUE
-	 */
-	private function setSrcInfo() {
-		// 分析图片信息
-		$imageInfo = getimagesize($this->srcfilename);
-		
-		// 支持的格式
-		if(empty($this->allowType[$imageInfo[2]])) {
-			$this->setOption('errorCode', -4);
-			return FALSE;
-		}
-		
-		// 设置属性值
-		if($this->srcsquare) {
-			$size = $imageInfo[0] > $imageInfo[1] ? $imageInfo[1] : $imageInfo[0];
-			$this->setOption('srcwidth', $size);
-			$this->setOption('srcheight', $size);
-		} else {
-			$this->setOption('srcwidth', $this->srcwidth ? : $imageInfo[0]);
-			$this->setOption('srcheight', $this->srcheight ? : $imageInfo[1]);
-		}
-		
-		$this->setOption('srcType', $this->allowType[$imageInfo[2]]);
-		
-		return TRUE;
-	}
-	
-	/**
-	 * 目标图片信息分析
-	 * @return boolean 分析成功返回TRUE
-	 */
-	private function setDistInfo() {
-		// 缩略图文件名
-		if(empty($this->distfilename)) {
-			$this->setOption('errorCode', -6);
-			return FALSE;
-		}		
 
-		// 格式检查
-		$disttype = $this->disttype ? : $this->srcType;
-		if(!in_array($disttype, $this->allowType)) {
-			$this->setOption('errorCode', -5);
-			return FALSE;
-		}
-		$this->setOption('disttype', $disttype);
-				
-		// 缩略图宽高度
-		switch(TRUE) {
-			case (!$this->distwidth && !$this->distheight):
-				// 没有设置大小，生成百分百大小
-				$this->setOption('distwidth', $this->srcwidth);
-				$this->setOption('distheight', $this->srcheight);
-				break;
-			case ($this->distwidth && !$this->distheight):
-				// 根据宽度计算大小
-				$distheight = floor(($this->distwidth/$this->srcwidth) * $this->srcheight);
-				$this->setOption('distheight', $distheight);
-				break;
-			case ($this->distheight && !$this->distwidth):
-				// 根据高度计算大小
-				$distwidth = floor(($this->distheight/$this->srcheight) * $this->srcwidth);
-				$this->setOption('distwidth', $distwidth);
-				break;
+		// 保存信息
+		$this->distImage['ext'] = $ext;
+		$this->distImage['path'] = $path;
+		$this->distImage['filename'] = $distFilename;
+	}
+
+	/**
+	 * 载入图片资源
+	 * @param string $filename 图片地址
+	 * @return resource
+	 */
+	private function openFile($filename) {
+		return imagecreatefromstring(file_get_contents($filename));
+	}
+
+	/**
+	 * 抛出异常信息
+	 * @throws \Exception
+	 * @return void
+	 */
+	private function throwError($errorCode = 0) {
+		$error = isset($this->errorNotify[$errorCode]) ? $this->errorNotify[$errorCode] : $this->errorNotify[0];
+		throw new \Exception($error, $errorCode);
+	}
+
+	/**
+	 * 图片输出或者进行保存
+	 * @param int $witdh 缩略长度
+	 * @param int $height 缩略高度
+	 * @param int $x 裁剪x轴坐标
+	 * @param int $y 裁剪y轴坐标
+	 * @return void
+	 */
+	private function saveOrOutput($width, $height, $startX, $startY, $endX, $endY) {
+		$ext = $this->distImage ? $this->distImage['ext'] : $this->srcImage['ext'];
+		if(!function_exists("image{$ext}")) {
+			$this->throwError(4);
 		}
 		
-		// 文件名保存
-		$disttype = ($disttype == 'jpeg') ? 'jpg' : $disttype;
-		$distfilename = rtrim($this->path, '/') . '/' . $this->distfilename . '.' . $disttype;
-		$this->setOption('distfilename', $distfilename);
+		$distResource = imagecreatetruecolor($width, $height);
+		imagesavealpha($this->srcImage['resource'], TRUE);
+		imagealphablending($distResource, FALSE);
+		imagesavealpha($distResource, TRUE);
+
+		$endX = $endX ?  : $this->srcImage['width'];
+		$endY = $endY ?  : $this->srcImage['height'];
+
+		imagecopyresampled($distResource, $this->srcImage['resource'], 0, 0,
+			$startX, $startY, $width, $height, $endX, $endY);
+
+		if($this->distImage) {
+			call_user_func("image{$ext}", $distResource, $this->distImage['filename']);
+		} else {
+			header("Content-Type: image/{$ext};charset=UTF-8");
+			call_user_func("image{$ext}", $distResource);
+		}
+
+		imagedestroy($this->srcImage['resource']);
+		imagedestroy($distResource);
+	}
+
+	/**
+	 * 通过设置width和height进行图像缩略
+	 * @throws \Exception
+	 * @param string $srcFilename 源图文件名
+	 * @param string $distFilename 保存文件名，如果直接输出设置成NULL
+	 * @param int $witdh 缩略长度
+	 * @param int $height 缩略高度
+	 * @param int $startX 裁剪x轴起始坐标
+	 * @param int $startY 裁剪y轴起始坐标
+	 * @param int $endX 裁剪x轴终点坐标
+	 * @param int $endY 裁剪y轴终点坐标
+	 * @return void
+	 */
+	public function byWidthHeight($srcFilename, $distFilename, $width, $height, $startX = 0, $startY = 0, $endX = NULL, $endY = NULL) {
+		$this->setSrcFilename($srcFilename);
+		$distFilename and $this->setDistFilename($distFilename);
 		
-		return TRUE;
+		$this->saveOrOutput($width, $height, $startX, $startY, $endX, $endY);
+	}
+
+	/**
+	 * 通过width或height的最大值进行图像等比缩略
+	 * @throws \Exception
+	 * @param string $srcFilename 源图文件名
+	 * @param string $distFilename 保存文件名，如果直接输出设置成NULL
+	 * @param int $size 缩略到的尺寸
+	 * @param int $startX 裁剪x轴起始坐标
+	 * @param int $startY 裁剪y轴起始坐标
+	 * @param int $endX 裁剪x轴终点坐标
+	 * @param int $endY 裁剪y轴终点坐标
+	 * @return void
+	 */
+	public function byMaxEdge($srcFilename, $distFilename, $size, $startX = 0, $startY = 0, $endX = NULL, $endY = NULL) {
+		$this->setSrcFilename($srcFilename);
+		$distFilename and $this->setDistFilename($distFilename);
+
+		$max = max($this->srcImage['width'], $this->srcImage['height']);
+		$proportion = $size / $max;
+		$width = floor($proportion * $this->srcImage['width']);
+		$height = floor($proportion * $this->srcImage['height']);
+
+		$this->saveOrOutput($width, $height, $startX, $startY, $endX, $endY);
 	}
 }
