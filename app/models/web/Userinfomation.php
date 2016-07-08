de14616a1312badfc5029880f8fb95f90997b554
陈晓波 <346745114@qq.com>
2016-07-08 17:46:41 +0800

    初始化框架

---
+++ app/models/web/Userinfomation.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * 用户信息表模型
+ * @author enychen
+ */
+namespace web;
+
+class UserinfomationModel extends \base\AbstractModel {
+
+	/**
+	 * 表名
+	 * @var string
+	 */
+	protected $table = 'user_infomation';
+}
