67cf425145634da604947d7bd779b85796b0689a
陈晓波 <346745114@qq.com>
2016-07-13 17:50:02 +0800

    初始化框架

---
+++ app/models/example/Example.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * 案例模型，用于参考
+ * @author enychen
+ */
+namespace example;
+
+class ExampleModel extends \base\AbstractModel {
+
+	/**
+	 * 表名
+	 * @var string
+	 */
+	protected $table = 'table_name';
+}
