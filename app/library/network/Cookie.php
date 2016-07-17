0852be48115b07fdd1f4d1eaa9e56e7ebbb56982
enychen <chenxiaobo_901021@yeah.net>
2016-07-17 15:41:45 +0800

    增加cookie类，删除header类中关于cookie和跳转的方法

---
+++ app/library/network/Cookie.php
@@ -0,0 +1,243 @@
+<?php
+
+/**
+ * cookie类
+ * @author enychen
+ */
+namespace network;
+
+class Cookie {
+
+	/**
+	 * cookie名称
+	 * @var string
+	 */
+	protected $name = NULL;
+
+	/**
+	 * cookie值
+	 * @var string
+	 */
+	protected $value = NULL;
+
+	/**
+	 * cookie过期时间
+	 * @var int
+	 */
+	protected $expire = 86400;
+
+	/**
+	 * cookie允许的目录
+	 * @var string
+	 */
+	protected $path = '/';
+
+	/**
+	 * cookie所属的域名
+	 * @var string
+	 */	
+	protected $domain = NULL;
+
+	/**
+	 * cookie是否使用https
+	 * @var boolean
+	 */
+	protected $secure = FALSE;
+
+	/**
+	 * cookie是否字段
+	 * @var string
+	 */
+	protected $httpOnly = TRUE;
+
+	/**
+	 * 设置cookie名称
+	 * @param string $name cookie名称
+	 * @return Cookie $this 返回当前对象进行连贯操作
+	 */
+	public function setName($name) {
+		$this->name = $name;
+		return $this;
+	}
+
+	/**
+	 * 获取cookie名称
+	 * @return string
+	 */
+	public function getName() {
+		return $this->name;
+	}
+
+	/**
+	 * 设置cookie值
+	 * @param string $value cookie值
+	 * @return Cookie $this 返回当前对象进行连贯操作
+	 */
+	public function setValue($value) {
+		$this->value = $value;
+		return $this;
+	}
+
+	/**
+	 * 获取cookie值
+	 * @return string
+	 */
+	public function getValue() {
+		return $this->value;
+	}
+
+	/**
+	 * 设置过期时间
+	 * @param int $expire 过期时间
+	 * @return Cookie $this 返回当前对象进行连贯操作
+	 */
+	public function setExpire($expire) {
+		$this->expire = $expire;
+		return $this;
+	}
+
+	/**
+	 * 获取过期时间
+	 * @return int
+	 */
+	public function getExpire() {
+		return $this->expire;
+	}
+
+	/**
+	 * 设置所属目录
+	 * @param string $path 所属目录
+	 * @return Cookie $this 返回当前对象进行连贯操作
+	 */
+	public function setPath($path) {
+		$this->path = $path;
+		return $this;
+	}
+
+	/**
+	 * 获取所属目录
+	 * @return string
+	 */
+	public function getPath() {
+		return $this->path;
+	}
+
+	/**
+	 * 设置所属域名
+	 * @param string $domain 所属域名
+	 * @param Cookie $this 返回当前对象进行连贯操作
+	 */
+	public function setDomain($domain) {
+		$this->domain = $domain;
+		return $this;
+	}
+
+	/**
+	 * 获取所属域名
+	 * @return string
+	 */
+	public function getDomain() {
+		return $this->domain;
+	}
+
+	/**
+	 * 设置是否使用https
+	 * @param boolean $secure 是否使用https
+	 * @return Cookie $this 返回当前对象进行连贯操作
+	 */
+	public function setSecure($secure) {
+		$this->secure = $secure;
+		return $this;
+	}
+
+	/**
+	 * 获取是否使用https
+	 * @return boolean
+	 */
+	public function getSecure() {
+		return $this->secure;
+	}
+
+	/**
+	 * 设置cookie是否只读
+	 * @param boolean $httpOnly 是否只读
+	 * @return Cookie $this 返回当前对象进行连贯操作
+	 */
+	public function setHttpOnly($httpOnly) {
+		$this->httpOnly = $httpOnly;
+		return $this;
+	}
+
+	/**
+	 * 获取cookie是否只读
+	 * @return boolean
+	 */
+	public function getHttpOnly() {
+		return $this->httpOnly;
+	}
+
+	/**
+	 * 抛出异常信息
+	 * @param string $message 异常信息
+	 * @param int $code 异常码
+	 * @return void
+	 * @throws \Exception
+	 */
+	protected function throws($message, $code = 0) {
+		throw new \Exception($message, $code);
+	}
+
+	/**
+	 * 增加cookie
+	 * @return boolean 设置成功返回TRUE，否则返回FALSE
+	 */
+	public function add() {
+		// 必备参数检查
+		if(!$this->getName()) {
+			$this->throws('请设置cookie名称');
+		}
+		if(!$this->getValue()) {
+			$this->throws('请设置cookie值');
+		}
+		if(!$this->getDomain()) {
+			$this->throws('请设置cookie域名');
+		}
+
+		// 要设置的cookie信息
+		$name = $this->getName();
+		$value = $this->getValue();
+		$expire = time() + $this->getExpire();
+		$path = $this->getPath();
+		$domain = $this->getDomain();
+		$secure = isset($_SERVER['REQUEST_SCHEME']) && (!strcasecmp($_SERVER['REQUEST_SCHEME'], 'https'));
+		$httpOnly = $this->gethttpOnly();
+
+		// 设置cookie
+		return setcookie($name, $value, $expire, $path, $domain, $secure, $httpOnly);
+	}
+
+	/**
+	 * 删除cookie
+	 * @return boolean 删除成功后返回TRUE，否则返回FALSE
+	 */
+	public function delete() {
+		// 必备参数检查
+		if(!$this->getName()) {
+			$this->throws('请设置cookie名称');
+		}
+		if(!$this->getDomain()) {
+			$this->throws('请设置cookie域名');
+		}
+
+		// 要删除的必备信息
+		$name = $this->getName();
+		$path = $this->getPath();
+		$expire = time() - $this->getExpire();
+		$domain = $this->getDomain();
+		$secure = isset($_SERVER['REQUEST_SCHEME']) && strcasecmp($_SERVER['REQUEST_SCHEME'], 'https');
+		$httpOnly = $this->gethttpOnly();
+
+		// 进行删除
+		return setcookie($name, NULL, $expire, $path, $domain, $secure, $httpOnly);
+	}
+}
--- app/library/network/Header.php
+++ app/library/network/Header.php
@@ -20,7 +20,7 @@ class Header {
 	/**
 	 * 跨域的访问控制可以跨域的域名
 	 * @static
-	 * @param string $domain 允许进行跨域的域名，多个域名用,隔开
+	 * @param string $domain 允许进行跨域的域名，多个域名用,隔开，必须设置完整到协议
 	 * @return void
 	 */
 	public static function accessControlAllowOrigin($domain = '*') {
@@ -34,42 +34,5 @@ class Header {
 	 */
 	public static function accessControlAllowHeaders($headers) {
 		header("Access-Control-Allow-Headers: {$headers}");
-	}
-
-	/**
-	 * 输出协议码信息
-	 * @static
-	 * @param int $code 协议码，目前只支持301,302,303,307,403,404
-	 * @param bool $exit 是否终止程序运行， 默认TRUE
-	 * @return void
-	 */
-	public static function httpCode($code, $exit = TRUE) {
-		$httpCode = array(
-			301=>"HTTP/1.1 301 Moved Permanently",
-			302=>"HTTP/1.1 302 Found",
-			303=>"HTTP/1.1 303 See Other",
-			307=>"HTTP/1.1 307 Temporary Redirect",
-			403=>'HTTP/1.1 403 Forbidden',
-			404=>'HTTP/1.1 404 Not Found',
-		);
-		
-		header($httpCode[$code]);
-		$exit and exit();
-	}
-	
-	/**
-	 * 设置cookie
-	 * @static
-	 * @param string $name cookie名称
-	 * @param string $value cookie值
-	 * @param int $expire 过期时间,直接写上加上或减少的时间，无需time()+3600这样
-	 * @param string $path  允许的目录,默认整站
-	 * @param string $domain 允许的域名,默认null表示当前域名
-	 * @param bool $secure 是否使用https，默认false
-	 * @param bool $httponly 是否cookie只读，默认true
-	 * @return void
-	 */
-	public static function setCookie($name, $value, $expire, $path='/', $domain=NULL, $secure=FALSE, $httponly=TRUE) {
-		setcookie($name, $value, (time() + $expire), $path, $domain, $secure, $httponly);
 	}
 }
