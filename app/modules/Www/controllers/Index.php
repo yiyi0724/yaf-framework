a2cd2c165c1aa24311e0ea8e232dc3d19b5fa4e6
陈晓波 <346745114@qq.com>
2016-07-20 14:56:09 +0800

    暂存

--- app/modules/Www/controllers/Index.php
+++ app/modules/Www/controllers/Index.php
@@ -10,27 +10,6 @@ class IndexController extends \base\WwwController {
 	 * 网站首页
 	 */
 	public function indexAction() {
-		$request = $this->getVailRequest();
-		$test = new \web\UserinfomationModel();
-		$pagitor = $test->order('uid DESC')->pagitor($request->get('p'), 15);
-		$this->assign('pagitor', $pagitor);
-	}
 
-	/**
-	 * 
-	 */
-	public function setcookieAction() {
-		\network\Header::p3p();
-		$cookieLib = new \network\Cookie();
-		$cookieLib->setExpire(3600);
-		$cookieLib->setName('_test');
-		$cookieLib->setValue('1');
-		$cookieLib->setDomain('.enychen.com');
-		$cookieLib->add();
-		exit;
-	}
-
-	public function getcookieAction() {
-		$this->debug($_COOKIE);
 	}
 }
