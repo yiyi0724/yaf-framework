efaffe63afd44f62038343d624d060420547374b
陈晓波 <89932976@qq.com>
2016-10-08 18:07:19 +0800

    初始化页面

---
+++ app/modules/Www/controllers/Game.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * 游戏控制器
+ * @author enychen
+ */
+class GameController extends \base\LogicController {
+
+	/**
+	 * 游戏排行
+	 */
+	public function rankAction() {
+	}
+
+    /**
+     * 进入游戏
+     */
+	public function playAction() {
+    }
+
+    /**
+     * 游戏详情
+     */
+    public function detailAction() {
+    }
+
+    /**
+     * 游戏分类
+     */
+    public function categoryAction() {
+    }
+}
