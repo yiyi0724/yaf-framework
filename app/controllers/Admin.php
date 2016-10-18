80c95f4ee0de104a71f78f12f908681d30fb0407
陈晓波 <89932976@qq.com>
2016-10-18 18:08:49 +0800

    用户权限

--- app/controllers/Admin.php
+++ app/controllers/Admin.php
@@ -15,6 +15,7 @@ abstract class AdminController extends BaseController {
     public function init() {
         $this->constInit();
         $this->loginInit();
+        $this->powerInit();
         $this->menusInit();
     }
 
@@ -30,9 +31,26 @@ abstract class AdminController extends BaseController {
                 $this->redirect('/login/');
                 break;
         }
-
         // 更新上次访问时间
         $this->getSession()->set('admin.time', time());
+
+
+    }
+
+    /**
+     * 检查用户访问权限
+     */
+    public function powerInit() {
+        // 检查用户权限
+        $menuModel = new \MenuModel();
+        $menuId = $menuModel->field('id')->where('module=:m and controller=:c and action=:a',
+            strtolower(MODULE_NAME),strtolower(CONTROLLER_NAME), strtolower(ACTION_NAME))
+            ->select()->fetchRow();
+
+        // 权限不足
+        if(ADMIN_RULES != '*' && !in_array($menuId, explode(',', ADMIN_RULES))) {
+            $this->redirect('/');
+        }
     }
 
     /**
@@ -45,6 +63,7 @@ abstract class AdminController extends BaseController {
         define('ADMIN_IP', $session->get('admin.ip'));
         define('ADMIN_TIME', $session->get('admin.time'));
         define('ADMIN_AVATAR', $session->get('admin.avatar'));
+        define('ADMIN_RULES', $session->get('admin.rules'));
     }
 
     /**
--- app/modules/Www/controllers/Login.php
+++ app/modules/Www/controllers/Login.php
@@ -28,7 +28,7 @@ class LoginController extends AdminController {
             $username = $request->get('username');
             $password = $request->get('password');
         } catch(\Exceptions\Multi $e) {
-            $this->json(FALSE, '手机号或密码格式不正确', 10102);
+            $this->json(FALSE, '手机号或密码格式不正确', 20001);
         }
 
         // 用户检查
@@ -36,16 +36,29 @@ class LoginController extends AdminController {
         $adminArr = $adminModel->where('username=:u and password=:p and status=1',
             $username,$adminModel->enctypePassword($password))->limit(1)->select()->fetchRow();
         if(!$adminArr) {
-            $this->json(FALSE, '账号或密码错误', 10101, $adminModel->enctypePassword($password));
+            $this->json(FALSE, '手机号或密码错误', 20002, $adminModel->enctypePassword($password));
         }
 
+        // 获取用户权限
+        $groupModel = new \GroupModel();
+        $groupArr = $groupModel->where('id=:id', $adminArr['group_id'])->select()->fetchRow();
+
+        // 计算用户权限
+        if($groupArr['id'] != '*') {
+            $rules = $adminArr['attach_rules'] ? explode(',', $adminArr['attach_rules']) : array();
+            $rules = implode(',', array_merge(explode(',', $groupArr['rules']), $rules));
+        }
+
+        // 保存用户权限
         $session = $this->getSession();
         $session->set('admin.uid', $adminArr['id']);
         $session->set('admin.name', $adminArr['nickname']);
         $session->set('admin.ip', IP::client());
         $session->set('admin.time', time());
         $session->set('admin.avatar', $adminArr['avatar']);
+        $session->set('admin.rules', implode(',', $rules));
 
-        $this->json(TRUE, '登录成功', 10001);
+        // 登录成功返回
+        $this->json(TRUE, '登录成功', 20010);
     }
 }
